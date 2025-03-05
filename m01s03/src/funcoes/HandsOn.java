e842b4dec01f24763bd2adb9740b453dbed4a7e5
Leonardo Penz <leonardopenz.ti@gmail.com>
2025-03-05 17:50:52 -0300

    explicação de funções na aula

--- m01s03/src/funcoes/HandsOn.java
+++ m01s03/src/funcoes/HandsOn.java
@@ -1,4 +1,23 @@
 package funcoes;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class HandsOn {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Informe o ano em que você nasceu: ");
+        int anoNasc = scan.nextInt();
+
+        int idade = calculaIdade(anoNasc);
+
+        System.out.printf("A sua idade é: %d anos", idade);
+    }
+
+    public static int calculaIdade(int anoNasc){
+        int dataAtual = LocalDate.now().getYear();
+        return dataAtual - anoNasc;
+
+    }
 }
--- m01s03/src/funcoes/Main.java
+++ m01s03/src/funcoes/Main.java
@@ -1,4 +1,60 @@
 package funcoes;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int valor = scan.nextInt();
+        //numeroPar(valor);
+
+        System.out.println(numeroPar(valor) ? "O número é PAR!" : "O número é IMPAR!");
+
+        String[] nomes = {"Alexandre", "Carlos", "Julia"};
+
+        String[][] produtos = {
+                {"Bicicleta", "Pino"},
+                {"Flauta", "Computador"}
+        };
+
+        ArrayList<String> emails = new ArrayList<>();
+        emails.add("teste@gmail.com");
+        emails.add("leonardopenz@gmail.com");
+
+        visualizarVetor(nomes);
+
+        exibirMatriz(produtos);
+
+        exibirArrayList(emails);
+    }
+
+    public static boolean numeroPar(int numero){
+        return numero % 2 == 0;
+
+    }
+
+    public static void visualizarVetor(String[] vars){
+
+        for(String var : vars){
+            System.out.println(var);
+        }
+    }
+
+    public static void exibirMatriz(String[][] vars){
+        for(String[] var : vars){
+            for(String value : var){
+                System.out.print(value + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void exibirArrayList(ArrayList<String> vars){
+        for(String var : vars){
+            System.out.println(var + "\t");
+        }
+    }
 }
